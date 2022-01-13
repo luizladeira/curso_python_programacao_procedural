f818162daba708a2da2020ed0244f4ccf5d64d97
Luiz Paulo Araujo Ladeira <A238349@ae.local>
2022-01-13 14:33:12 -0300

    Funções

--- aula01/aula01.py
+++ aula01/aula01.py
@@ -1,7 +1,48 @@
 """
 FUNÇÕES: def - definir funções próprias - PART 01
 """
-def funcao():
-    print('Olá, tudo bem?')
+def saudacao(msg="Olá", nome="Rodolfo"):
+    nome = nome.replace('e', '3')
+    print(msg, nome)
 
-funcao()
+saudacao('Mensagem', 'Luiz Ladeira')
+saudacao()
+saudacao(nome="Zezínho")
+
+def teste2(msg="Olá", nome="Rodolfo"):
+    nome = nome.replace('e', '3')
+    return f'{msg} {nome}'
+
+variavel = teste2()
+print(variavel)
+
+def divisao(n1, n2):
+    if n2 == 0:
+        return
+    return n1 / n2
+
+dividir = divisao(0,2)
+if dividir:
+    print(dividir)
+else:
+    print('Conta inválida')
+
+def dumb():
+    return [2, '2', 77, True]
+var1 = dumb()
+print(var1, type(var1))
+
+def f(var):
+    print(var)
+
+def fdumb():
+    return f
+print(fdumb)
+var = fdumb()('E colocar o valor aqui')
+
+# Função retorna tuplas - que são listas que não podem ser alteradas
+def tuplas():
+    return 'Luiz', 'Ladeira'
+print(tuplas())
+var = tuplas()
+print(var, type(var))
